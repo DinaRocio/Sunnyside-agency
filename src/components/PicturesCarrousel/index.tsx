3fef5b4a445c7dd11c786836e1730acfa5c92704
Dina Villanueva <dinarociovq15@hotmail.com>
2022-01-05 23:48:05 -0500

    add new pictures section (responsive)

---
+++ src/components/PicturesCarrousel/index.tsx
@@ -0,0 +1,41 @@
+import milkbottlesDK from "./../../assets/desktop/image-gallery-milkbottles.jpg";
+import orangeDK from "./../../assets/desktop/image-gallery-orange.jpg";
+import coneDK from "./../../assets/desktop/image-gallery-cone.jpg";
+import sugarcubesDK from "./../../assets/desktop/image-gallery-sugarcubes.jpg";
+import styled from "@emotion/styled";
+
+interface PictureProps {
+  image: string;
+}
+
+const PicturesContainer = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Picture = styled.div<PictureProps>`
+  background-image: url(${(props) => props.image});
+  height: 550px;
+  width: 25%;
+
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  @media only screen and (max-width: 600px) {
+    height: 300px;
+    width: 50%;
+  }
+`;
+
+function PicturesCarrousel() {
+  return (
+    <PicturesContainer>
+      <Picture image={milkbottlesDK} className="reverse-image" />
+      <Picture image={orangeDK} className="reverse-image" />
+      <Picture image={coneDK} className="reverse-image" />
+      <Picture image={sugarcubesDK} className="reverse-image" />
+    </PicturesContainer>
+  );
+}
+
+export default PicturesCarrousel;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import Header from "../../components/Header";
 import Services from "../../components/Services";
 import Testimonies from "../../components/Testimonies";
+import PicturesCarrousel from "../../components/PicturesCarrousel";
 import Footer from "../../components/Footer";
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <Header />
       <Services />
       <Testimonies />
+      <PicturesCarrousel />
       <Footer />
     </>
   );
